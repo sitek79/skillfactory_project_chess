69b929ee40f220496d89d6fe6ebfcbd4d2bbee7e
sitek79 <atikhonov79@gmail.com>
2021-10-19 21:14:25 +0300

    Add Class Horse. Branch class_horse

---
+++ src/projectchess/Horse.java
@@ -0,0 +1,21 @@
+package projectchess;
+
+public class Horse {
+    String color;
+    public Horse(String color) {
+        this.color;
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    boolean canMoveToPosition() {
+        return true;
+    }
+
+    String getSymbol() {
+        return "H";
+    }
+}
+
