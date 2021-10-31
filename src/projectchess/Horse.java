6d90a440ae6ed6cb287a3a538faa0dda5e36efef
sitek79 <atikhonov79@gmail.com>
2021-10-31 20:35:10 +0300

    20okt2021

--- src/projectchess/Horse.java
+++ src/projectchess/Horse.java
@@ -1,20 +1,49 @@
 package projectchess;
 
-public class Horse {
-    String color;
+public class Horse extends ChessPiece {
+
     public Horse(String color) {
-        this.color
+        super(color);
     }
 
-    String getColor() {
-        return color;
+    @Override
+    public String getColor() {
+        return this.color;
     }
 
-    boolean canMoveToPosition() {
-        return true;
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        // все координаты существуют
+        if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn)) {
+            // стартовая координата не равна конечной
+            if (line != toLine && column != toColumn &&
+                    // и конечная клетка пустая или цвет фигуры в конечной клетке не равен текущему
+                    (chessBoard.board[toLine][toColumn] == null || !chessBoard.board[toLine][toColumn].color.equals(this.color))
+                    // и стартовая клетка не пустая
+                    && chessBoard.board[line][column] != null) {
+                    // если стартовая клетка не равна коню, то не ходим
+                    if (!chessBoard.board[line][column].equals(this)) {
+                        return false;
+                    }
+                    // перечислим все возможные позиции для лошади
+                    int[][] positions = new int[][]{
+                            {line - 2, column - 1}, {line - 2, column + 1},
+                            {line + 2, column - 1}, {line + 2, column + 1},
+                            {line - 1, column - 2}, {line - 1, column + 2},
+                            {line + 1, column - 2}, {line + 1, column + 2},
+                    };
+                    // проверяем, можно ли сходить в нужную позицию
+                    for (int i = 0; i < positions.length; i++) {
+                        if (positions[i][0] == toLine && positions[i][1] == toColumn)
+                            return true;
+                    }
+            }
+        } else return false;
+        return false;
     }
 
-    String getSymbol() {
+    @Override
+    public String getSymbol() {
         return "H";
     }
 }
