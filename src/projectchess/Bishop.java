57d54f4e97b0cca694d0c05d54452a1d1006e151
sitek79 <atikhonov79@gmail.com>
2021-11-02 15:30:53 +0300

    02Nov2021. Class Bishop OK

--- src/projectchess/Bishop.java
+++ src/projectchess/Bishop.java
@@ -8,16 +8,64 @@ public class Bishop extends ChessPiece {
 
     @Override
     public String getColor() {
-        return null;
+        return this.color;
     }
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return false;
+        // начальная точка не равна конечной
+        if (line != toLine && column != toColumn &&
+                // и ходит по диагонали
+                getMax(line, toLine) - getMin(line, toLine) == getMax(column, toColumn) - getMin(column, toColumn) &&
+                // и все координаты существуют
+                checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn) &&
+                // и конечная точка пустая или на ней стоит фигура другого цвета
+                (chessBoard.board[toLine][toColumn] == null || !chessBoard.board[toLine][toColumn].color.equals(this.color)) &&
+                // и стартовая клетка не пустая
+                chessBoard.board[line][column] != null) {
+            // сверху слева -> направо вниз
+            if ((column == getMin(column, toColumn) && line == getMax(line, toLine)) ||
+                    (toColumn == getMin(column, toColumn) && toLine == getMax(line, toLine))) {
+                // Max и Min нужны чтобы можно было делать обратный ход (т.е. сверху справа -> вниз налево)
+                int fromL = getMax(line, toLine);
+                int fromC = getMin(column, toColumn);
+                int toL = getMin(line, toLine);
+                int toC = getMax(column, toColumn);
+                // позиции, которые слон проходит по пути
+                int[][] position = new int[toC - fromC][1];
+                // число колонок = числу линий пройденных слоном
+                for (int i = 1, i < toC - fromC; i++) {
+                    if (chessBoard.board[fromL - i][fromC + i] == null) {
+                        position[i - 1] = new int[] {fromL - i, fromC + i};
+                    } else if (!chessBoard.board[fromL - i][fromC + i].color.equals(this.color) && fromL - i == toLine) {
+                        position[i - 1] = new int[] {fromL - i, fromC + i};
+                    } else  {
+                        return false;
+                    }
+                }
+                return true;
+            } else { // сверху справа -> налево вниз
+                int fromL = getMin(line, toLine);
+                int fromC = getMin(column, toColumn);
+                int toL = getMax(line, toLine);
+                int toC = getMax(column, toColumn);
+                int[][] position = new int[toC-fromC][1];
+                for (int i = 1; i < toC - fromC; i++) {
+                    if (chessBoard.board[fromL + i][fromC + i] == null) {
+                        position[i-1] = new int[] {fromL + i, fromC + i};
+                    } else if (!chessBoard.board[fromL + i][fromC + i].color.equals(this.color) && fromL + i == toLine) {
+                        position[i-1] = new int[] {fromL + i, fromC + i};
+                    } else {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        } else return false;
     }
 
     @Override
     public String getSymbol() {
-        return null;
+        return "B";
     }
 }
